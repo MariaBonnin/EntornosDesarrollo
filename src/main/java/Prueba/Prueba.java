04ae48ec5f1441727c9ecd6855a7a620c5502ce2
Miguel MR <miguelmr90@hotmail.com>
2021-03-18 12:55:55 +0100

    Probando GitHub

---
+++ src/main/java/Prueba/Prueba.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Prueba;
+
+/**
+ *
+ * @author migue
+ */
+public class Prueba {
+    
+    public static void main(String args []){
+        int num =3;
+        System.out.println(num);
+    }
+    
+}
